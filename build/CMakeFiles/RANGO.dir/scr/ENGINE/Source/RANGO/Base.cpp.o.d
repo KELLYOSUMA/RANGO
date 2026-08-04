00cd6581d6efcf707e2f464e7430a447185bd664
PeterMathaiMaina <zephep37@gmail.com>
2026-08-04 13:02:24 +0300

    started in the graphics context thing

--- build/CMakeFiles/RANGO.dir/scr/ENGINE/Source/RANGO/Base.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/RANGO.dir/scr/ENGINE/Source/RANGO/Base.cpp.o: \
- /home/kelly/RANGO/scr/ENGINE/Source/RANGO/Base.cpp \
- /usr/include/stdc-predef.h \
- /home/kelly/RANGO/scr/ENGINE/Include/RANGO/Base.h
